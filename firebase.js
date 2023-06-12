8a6cca2f60bdbf054664589e4c2b10b27496489f
mellb26 <102174403+mellb26@users.noreply.github.com>
2023-06-12 10:11:44 +0100

    now added firebase

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQEQKevUKp-mnS2Du13_uTpDuBHD4qfpQ",
+  authDomain: "park-me-bike.firebaseapp.com",
+  projectId: "park-me-bike",
+  storageBucket: "park-me-bike.appspot.com",
+  messagingSenderId: "857359915558",
+  appId: "1:857359915558:web:a5bc2a28b7d002d0f2e4e7",
+  measurementId: "G-DYYTZFJSCT",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
